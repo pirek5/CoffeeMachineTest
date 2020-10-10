0e99251797acdc6fa64cbf2f8160994e61693616
Piotr Czajkowski <piotr.czajkowski@polyslash.com>
2020-10-10 10:42:49 +0200

    add: basic data structures

---
+++ Assets/Scripts/Coffee.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CoffeeSize
+{
+    Small,
+    Medium,
+    Big,
+};
+
+public enum CoffeeStrength
+{
+    Mild,
+    Normal,
+    Strong,
+}
+
+public class Coffee
+{
+    public CoffeeStrength coffeeStrength;
+    public CoffeeSize coffeeSize;
+    public float amountOfWater;
+    public float usedCoffeeSeeds;
+    public float producedCoffeeGrounds;
+}
---
+++ Assets/Scripts/CoffeeMachineState.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "CoffeeMachineState", menuName = "CoffeeMachine/CoffeeMachineState")]
+public class CoffeeMachineState : ScriptableObject
+{
+   public float amountOfWaterInTank;
+   public float amountOfWaterInTray;
+   public float amountOfCoffee;
+   public float amountOfCoffeeGrounds;
+}
