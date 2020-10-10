bc6aa694df5e258edc724c364e692bd8a2afc225
Piotr Czajkowski <piotr.czajkowski@polyslash.com>
2020-10-10 17:05:18 +0200

    add: coffee machine editor window controller

---
+++ Assets/Scripts/Editor/CoffeeMachineEditorView.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine.UIElements;
+
+public class CoffeeMachineEditorView
+{
+    private readonly VisualElement root;
+    private readonly CoffeeMachine coffeeMachine;
+
+    private TextField status;
+    
+    public CoffeeMachineEditorView(VisualElement root, CoffeeMachine coffeeMachine)
+    {
+        this.root = root;
+        this.coffeeMachine = coffeeMachine;
+    }
+
+    public void Show(VisualTreeAsset template)
+    {
+        template.CloneTree(root);
+        AssignStatusTextField();
+        AssignActionsButtons();
+        AssignCoffeeMachineButtons();
+    }
+
+    private void AssignStatusTextField()
+    {
+        status = root.Q<TextField>("StatusTextField");
+    }
+
+    private void AssignActionsButtons()
+    {
+        root.Q<Button>("CheckMachineButton").clicked += () =>
+        {
+            status.value = coffeeMachine.CheckMachineState();
+        };
+        root.Q<Button>("RefillWaterTankButton").clicked += coffeeMachine.RefillWater;
+        root.Q<Button>("RefillCoffeeTankButton").clicked += coffeeMachine.RefillCoffee;
+        root.Q<Button>("RemoveWaterFromTrayButton").clicked += coffeeMachine.RemoveWaterFromTray;
+        root.Q<Button>("RemoveCoffeeGroundsButton").clicked += coffeeMachine.RemoveCoffeeGrounds;
+    }
+
+    private void AssignCoffeeMachineButtons()
+    {
+        root.Q<Button>("OnButton").clicked += () =>
+        {
+            coffeeMachine.ToggleMachine(true);
+        };
+        root.Q<Button>("OffButton").clicked += () =>
+        {
+            coffeeMachine.ToggleMachine(false);
+        };
+        
+        root.Q<Button>("MildButton").clicked += () =>
+        {
+            coffeeMachine.ChangeCoffeeType(CoffeeStrength.Mild);
+        };
+        root.Q<Button>("NormalButton").clicked += () =>
+        {
+            coffeeMachine.ChangeCoffeeType(CoffeeStrength.Normal);
+        };
+        root.Q<Button>("StrongButton").clicked += () =>
+        {
+            coffeeMachine.ChangeCoffeeType(CoffeeStrength.Strong);
+        };
+        
+        root.Q<Button>("SmallButton").clicked += () =>
+        {
+            coffeeMachine.ChangeCoffeeSize(CoffeeSize.Small);
+        };
+        root.Q<Button>("MediumButton").clicked += () =>
+        {
+            coffeeMachine.ChangeCoffeeSize(CoffeeSize.Medium);
+        };
+        root.Q<Button>("BigButton").clicked += () =>
+        {
+            coffeeMachine.ChangeCoffeeSize(CoffeeSize.Big);
+        };
+
+        root.Q<Button>("MakeCoffeeButton").clicked += () =>
+        {
+            status.value = coffeeMachine.MakeCoffee().ToString();
+        };
+    }
+}
---
+++ Assets/Scripts/Editor/CoffeeMachineWindow.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CoffeeMachineWindow : EditorWindow
+{
+    [SerializeField] private CoffeeMachineSettings coffeeMachineSettings;
+    [SerializeField] private CoffeeMachineState coffeeMachineState;
+    
+    private readonly string windowTemplatePath = "Assets/Data/UiWindows/CoffeeMachineTemplate.uxml";
+    
+    [MenuItem("CoffeeMachine/CoffeeMachineWindow")]
+    public static void ShowWindow()
+    {
+        var window = GetWindow<CoffeeMachineWindow>("Coffee Machine Window");
+        window.minSize = new Vector2(500f, 300f);
+    }
+
+    private void OnEnable()
+    {
+        CoffeeMachine coffeeMachine = new CoffeeMachine(coffeeMachineState, coffeeMachineSettings);
+        CoffeeMachineEditorView coffeeMachineEditorView = new CoffeeMachineEditorView(rootVisualElement, coffeeMachine);
+        var windowTreeAsset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(windowTemplatePath);
+        coffeeMachineEditorView.Show(windowTreeAsset);
+    }
+}
